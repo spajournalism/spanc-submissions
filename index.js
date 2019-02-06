1496ec4be0afba9d8c8c3e7b3910f14d06bb09fc
Elliot Davies <elliot.a.davies@gmail.com>
2019-02-06 14:14:35 +0000

    Further sort entries depending on category (fix #2)

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ function handleFormSubmission(e) {
 
   var awardsFolder = getOrCreateFolder(SPANCFolderName, DriveApp);
   var categoryFolder = getOrCreateFolder(category, awardsFolder);
+  var subFolder = getOrCreateFolder(entrantOrPublication(category, values), categoryFolder);
 
   ids.forEach(function(id) {
     var file = DriveApp.getFileById(id);
-    categoryFolder.addFile(file);
+    subFolder.addFile(file);
   });
 }
 
@@ -38,4 +39,39 @@ function parseFileNames(str) {
 
 function parseId(s) {
   return s.split("=")[1];
+}
+
+function entrantOrPublication(category, values) {
+  var entrant = values["Name of entrant"][0];
+  var publication = values["Publication"][0];
+  
+  switch (category) {
+    case "Best Publication":
+    case "Best Website":
+    case "Best Newspaper Design":
+    case "Best Magazine Design":
+    case "Best Overall Digital Media":
+    case "Best Sports Coverage":
+    case "Best Newcomer Publication":
+    case "Best Specialist Publication":
+    case "Best Science Publication or Section":
+      return publication;
+     
+    case "Billy Dowling-Reid Award for Outstanding Commitment":
+    case "Best Reporter":
+    case "Best Lifestyle Story":
+    case "Best Science/Technology Story":
+    case "Best Human Rights Journalist":
+    case "Best Human Rights Story":
+    case "Best News Story":
+    case "Best Feature":
+    case "Best Interview":
+    case "Best Entertainment Piece":
+    case "Best Comment or Opinion":
+    case "Best Student Photographer":
+      return entrant;
+    
+    default:
+      return entrant;
+  }
 }
